bd0f9661f841f631a1e8fa4de87081187547ed73
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2023-11-17 22:11:56 +0300

    Update code.cpp

--- ETS0918_15_METI_SEBOKA/Q3.letter case conversion/code.cpp
+++ ETS0918_15_METI_SEBOKA/Q3.letter case conversion/code.cpp
@@ -1,1 +1,16 @@
+// letter case conversion program
+#include <iostream>
+using namespace std;
+int main(){
+char letter;
+    cout << "Enter a letter: ";
+    cin >> letter;
 
+    if (islower(letter)) {
+        cout << "The uppercase equivalent is: " << static_cast<char>(toupper(letter)) << endl;
+    } else if (isupper(letter)) {
+        cout << "The lowercase equivalent is: " << static_cast<char>(tolower(letter)) << endl;
+    } else {
+        cout << "Invalid input. Please enter a letter." << endl;
+      return 0;
+    }
