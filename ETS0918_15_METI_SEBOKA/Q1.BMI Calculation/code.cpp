b48834872425d128967d0af91cb0b00f28e46d4a
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2023-11-17 21:34:45 +0300

    Update code.cpp

--- ETS0918_15_METI_SEBOKA/Q1.BMI Calculation/code.cpp
+++ ETS0918_15_METI_SEBOKA/Q1.BMI Calculation/code.cpp
@@ -1,1 +1,13 @@
-
+//BMI Calculation program
+#include<iostream>
+using namespace std;
+int main(){   
+  double weight, height, BMI;
+     /*cout << " enter weight in kilograms: ";
+    cin >> weight;
+    cout << " enter your height in meters: ";
+    cin >> height;
+    BMI = weight / (height * height);//calculate BMI
+   cout << "the BMI is: " << BMI;
+return 0;
+}
