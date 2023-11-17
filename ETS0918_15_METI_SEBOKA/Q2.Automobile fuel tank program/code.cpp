bd30ad1cc0028a23ab11bd84f9a3ad0e4e857db0
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2023-11-17 21:55:00 +0300

    Update code.cpp

--- ETS0918_15_METI_SEBOKA/Q2.Automobile fuel tank program/code.cpp
+++ ETS0918_15_METI_SEBOKA/Q2.Automobile fuel tank program/code.cpp
@@ -1,1 +1,15 @@
+//Automobile fuel tank program
+#include <iostream>                                                                                                                                                                                              
+using namespace std;
+int main(){
+float capacity, milesPerGallon;
 
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> capacity;
+    cout << "Enter the miles per gallon the automobile can be driven: ";
+    cin >> milesPerGallon;
+
+    float milesWithoutRefueling = capacity * milesPerGallon;
+    cout << "The automobile can be driven " << milesWithoutRefueling << " miles without refueling." << endl;
+return 0;
+}
