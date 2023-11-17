555a10f3d0021b0559f854c4fd3fb2f9d7a7d5f9
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2023-11-17 22:52:05 +0300

    Update code.cpp

--- ETS0918_15_METI_SEBOKA/Q6.Time taken program/code.cpp
+++ ETS0918_15_METI_SEBOKA/Q6.Time taken program/code.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+double timeInSec, timeInMinute, timeInHr, sizeInByte;
+
+    cout << "Input the file size in bytes: ";
+    cin >> sizeInByte;
+
+    timeInSec = sizeInByte / 960; //960 characters = 960 bytes
+    timeInMinute = timeInSec / 60;
+    timeInHr = timeInMinute / 60;
+    
+    cout << "Time needed in seconds is: " << timeInSec << '\n';
+    cout << "Time needed in Minutes is: " << timeInMinute << '\n';
+    cout << "Time needed in Hours is: " << timeInHr;
+
 int main()
 {
 
