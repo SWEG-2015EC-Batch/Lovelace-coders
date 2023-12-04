dadfb9f4c8c17533a8e6300ba8af9637124f5cec
mariamawitelias <149385188+mariamawitelias@users.noreply.github.com>
2023-12-04 10:41:41 +0300

    Add files via upload

---
+++ ETS0872_15_Mariamawit_Elias/Activity 3.1/simple calculator.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int  main() {
+  double num1, num2;
+  double exit;
+  char op;
+  do {
+    cout << "enter num1:" << endl;
+
+    cin >> num1;
+    cout << "enter num2:" << endl;
+
+    cin >> num2;
+    //op stands for operator.
+    cout << "enter the operator" << endl;
+    cin >> op;
+
+
+    switch (op)
+    {
+    case'+':
+      cout << "the sum of this two numbers is:" << num1 + num2 << endl;
+      break;
+    case'-':
+      cout << "the difference of this two number is:" << num1 - num2 << endl;
+      break;
+    case '*':
+      cout << "the product of this two numbers is:" << num1 * num2 << endl;
+      break;
+    case'/':
+      cout << "the division of this two number is:" << num1 / num2 << endl;
+      break;
+
+
+    default:
+      break;
+    }
+    cout << "to exit enter 0" << endl;
+    cin >> exit;
+    if (exit == 0)
+      cout << "exit" << endl;
+
+  } while (exit != 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
