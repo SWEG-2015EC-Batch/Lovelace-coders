5c338d92977526a0542d162e8a733c7d161388fc
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2023-12-18 09:29:31 +0300

    Create count_no_of_digits.cpp

---
+++ ETS0916_15_mesud_melaku/activity 3.1/playing with digit/count_no_of_digits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    int count = 0;
+    
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    if (number == 0) {
+        count = 1;
+    } else {
+        while (number != 0) {
+            number /= 10; 
+            count++; 
+        }
+    }
+    
+    cout << "Number of digits: " << count << endl;
+    
+    return 0;
+}
