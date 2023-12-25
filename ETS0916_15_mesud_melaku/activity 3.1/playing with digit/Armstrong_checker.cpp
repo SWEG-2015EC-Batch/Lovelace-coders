6e80f5ac320d9e47265421d583532382818eae47
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2023-12-25 20:13:16 +0300

    Update Armstrong_checker.cpp

--- ETS0916_15_mesud_melaku/activity 3.1/playing with digit/Armstrong_checker.cpp
+++ ETS0916_15_mesud_melaku/activity 3.1/playing with digit/Armstrong_checker.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int number;
     cout << "Enter a number: ";
     cin >> number;
 
     int originalNumber = number;
     int sum = 0;
-    int digits = 0;
+    int noOfDigits = 0;
 
-    while (number != 0) {
+    while (number != 0)
+    {
         noOfDigits++;
         number /= 10;
     }
 
     number = originalNumber;
 
-    while (number != 0) {
+    while (number != 0)
+    {
         int digit = number % 10;
         int power = 1;
 
         // Calculate power of digit using a loop
-        for (int i = 0; i < noOfDigits; i++) {
+        for (int i = 0; i < noOfDigits; i++)
+        {
             power *= digit;
         }
 
@@ -30,9 +34,12 @@ int main() {
         number /= 10;
     }
 
-    if (sum == originalNumber) {
+    if (sum == originalNumber)
+    {
         cout << originalNumber << " is an Armstrong number." << endl;
-    } else {
+    }
+    else
+    {
         cout << originalNumber << " is not an Armstrong number." << endl;
     }
 
