48e3ffd7f724291e7a81e5e59fa53d66c523803a
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2024-01-14 00:24:54 -0800

    Create Q3.cpp

---
+++ ETS0918_15_METI_SEBOKA/Activity 4.2/String Practical Exersises/Q3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+     
+    string word = "miki:ya$s'm*ech#a=*l/-o@gmail.com";
+    
+    for (int i = 0; i < word.length(); i++){
+        if(!isalpha(word[i])){
+            word.erase(i,1);
+            i--;
+        }a
+    }
+    cout << "The remaining charachters are : " << word; 
+    
+
+
+    return 0;
+}
