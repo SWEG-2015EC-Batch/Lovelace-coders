f5dc5610fa8d60504b0617e7fae647e642218bf4
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2024-01-14 00:23:08 -0800

    Update Q1.cpp

--- ETS0918_15_METI_SEBOKA/Activity 4.2/String Practical Exersises/Q1.cpp
+++ ETS0918_15_METI_SEBOKA/Activity 4.2/String Practical Exersises/Q1.cpp
@@ -1,8 +1,41 @@
-for ( int j=5; j>=1;j--){
-for( int i=1; i<=j; i++)
-{
+#include <iostream>
+#include <cstring>
+using namespace std;
 
-cout<<"*";
+int main(){
 
+    string word;
+    cout << "Enter a word or a sentence: ";
+    getline(cin, word);
+    
+    int vowel= 0, consonant = 0;
+
+    for (int i=0; i<word.length(); i++){
+        char temp = toupper(word[i]);
+        switch (temp){
+        case 'A':
+            vowel +=1;
+            break;
+        case 'E':
+            vowel +=1;
+            break;
+        case 'I':
+            vowel +=1;
+            break;
+        case 'O':
+            vowel +=1;
+            break;
+        case 'U':
+            vowel +=1;
+            break;
+        default:
+            consonant +=1;
+            break;
+        }
+    }
+
+    cout << "We have " << vowel << " vowel letters";
+    cout << " and " << consonant << " consonant letters.\n";
+    
+    return 0;
 }
-cout<<endl;
