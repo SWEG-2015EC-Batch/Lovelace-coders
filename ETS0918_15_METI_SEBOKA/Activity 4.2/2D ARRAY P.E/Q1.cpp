1a666bff50f05f60df1c7bfe7979cccda5283a51
Metiseboka <149230080+Metiseboka@users.noreply.github.com>
2024-01-14 00:18:39 -0800

    Update Q1.cpp

--- ETS0918_15_METI_SEBOKA/Activity 4.2/2D ARRAY P.E/Q1.cpp
+++ ETS0918_15_METI_SEBOKA/Activity 4.2/2D ARRAY P.E/Q1.cpp
@@ -1,8 +1,28 @@
-for ( int j=5; j>=1;j--){
-for( int i=1; i<=j; i++)
-{
+#include <iostream>
 
-cout<<"*";
+using namespace std;
 
+int main(){
+
+    int first [] = {12, 25, 35, 41};
+    // int second [] = {12, 25, 35, 41};
+    int second[] = {12, 25, 35, 41};
+
+    for(int i=0; i<4; i++){
+        if (first[i]==second[i])
+        {
+            continue;
+        }
+        else{
+            cout << "The two arrays are not the same.";
+            return 0;
+        }
+        
+    }
+    cout << "The two arrays are the same";
+ 
+
+
+    return 0;
+    
 }
-cout<<endl;
